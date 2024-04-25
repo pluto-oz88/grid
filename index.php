0efdb7253322f98c06f73898ef8c2358b3ea1909
Graham Dunsby <gdunsby@gmail.com>
2024-04-26 08:23:54 +1000

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Grid Debugger</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/grid.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="gridA">
+      <div class="picL textone">
+        <h1>text one</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque eos sint soluta! Ad, eos quasi! Explicabo nemo, provident soluta sit tenetur sed numquam placeat repellat maiores eligendi totam natus! Impedit adipisci reprehenderit numquam corrupti fugiat id itaque quasi voluptates labore!</p>
+      </div>
+      <div class="picR2 pic2Right">
+        <h1>Nusa Penida</h1>
+        <img class="wrapped" src="https://fakeimg.pl/1400x800/f0d000,128/000,255/?text=Wrapping Left">
+        <h2>How to get here</h2>
+        <p>Start saving now. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, velit sit, iure a nesciunt architecto aut suscipit similique tenetur dolore deserunt debitis praesentium nulla cupiditate aspernatur, ducimus temporibus ipsa esse tempora laudantium. Similique eveniet voluptate cumque suscipit reprehenderit, ullam maiores quam laudantium labore temporibus eaque et! Laborum totam cupiditate adipisci quo nihil explicabo. Laborum, vel cumque corrupti beatae dolorum quaerat tenetur iure nesciunt. Unde molestias enim aspernatur ratione? Quam repudiandae hic officiis ratione animi incidunt repellendus itaque, totam vero perferendis quia cum delectus. At incidunt, eum aliquid numquam tempora excepturi temporibus voluptatum adipisci accusantium fugiat consectetur repellat. Corporis, facere voluptas!</p>
+
+      </div>
+      <div class="cell"><img class="wrapped" src="https://fakeimg.pl/1400x800/f0d000,128/000,255/?text=Picture only"></div>
+      <div class="textone">
+        <h1>Get a good flight cell 15</h1>
+        <h2>buy cheap seats</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam et magnam delectus velit amet nostrum fugiat animi ipsa ratione reprehenderit consequuntur repellendus voluptate omnis rerum voluptatum ex labore minima facilis, iste maxime eveniet quis, itaque blanditiis beatae! Quaerat similique ducimus ea, sunt velit quisquam neque reiciendis aperiam nam voluptatum recusandae odio nulla consequuntur sed soluta autem exercitationem corporis quia eos.</p>
+      </div>
+      <div class="cell">cell 17</div>
+      <div class="cell">cell 18</div>
+      <div class="picL3"><img class="wrapped" src="https://fakeimg.pl/1400x800/ffdff0,128/000,255/?text=3 Col Photo picL3"></div>
+      <div class="picL2 pic2Left">
+        <h1>Noosaville</h1>
+        <img class="wrapped" src="https://fakeimg.pl/1400x800/f0d000,128/000,255/?text=Wrapping Right">
+        <h2>How to get here</h2>
+        <p>Start saving now. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, velit sit, iure a nesciunt architecto aut suscipit similique tenetur dolore deserunt debitis praesentium nulla cupiditate aspernatur, ducimus temporibus ipsa esse tempora laudantium. Similique eveniet voluptate cumque suscipit reprehenderit, ullam maiores quam laudantium labore temporibus eaque et! Laborum totam cupiditate adipisci quo nihil explicabo. Laborum, vel cumque corrupti beatae dolorum quaerat tenetur iure nesciunt. Unde molestias enim aspernatur ratione? Quam repudiandae hic officiis ratione animi incidunt repellendus itaque, totam vero perferendis quia cum delectus. At incidunt, eum aliquid numquam tempora excepturi temporibus voluptatum adipisci accusantium fugiat consectetur repellat. Corporis, facere voluptas!</p>
+      </div>
+      <div class="picR textone">
+        <h1>Single box text</h1>
+        <h2>plain text box</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nostrum totam cupiditate velit excepturi voluptates ducimus, atque quisquam? Nesciunt quasi, impedit veritatis animi modi quidem voluptas dolorum unde corporis non possimus quae fugiat eligendi ipsa, at culpa accusantium officiis natus corrupti doloremque. Esse soluta explicabo tempore repellat, cumque voluptatem, obcaecati iusto corporis impedit, aliquid neque deleniti accusantium doloremque voluptatibus ea.</p>
+      </div>
+      <div class="cell">cell 18</div>
+      <div class="cell">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati ipsum odit assumenda quod modi saepe iusto unde totam, qui sint fugiat. Officiis dolorem nostrum aut laudantium sunt! Et quam suscipit voluptate magnam, neque similique possimus architecto quidem unde, eaque dolores doloribus voluptates quisquam animi blanditiis impedit, sed mollitia quaerat non incidunt! Quasi ullam repellendus quae. Voluptatibus ipsa nemo voluptate nisi eos debitis obcaecati nulla iste beatae exercitationem similique molestias corporis, sequi necessitatibus quo distinctio veniam. Rem nam assumenda beatae non cupiditate nisi, rerum odio eos ut in libero! Mollitia nemo asperiores sed, consequatur dicta maxime, voluptatem alias corporis error modi sequi. Esse, quae! Nemo expedita eum possimus ipsam enim voluptatum itaque magnam sequi! A tempore deserunt ea nostrum modi?</div>
+      <div class="cell">cell 20</div>
+      <div class="picR3"><img class="wrapped" src="https://fakeimg.pl/1400x800/ffd000,128/000,255/?text=3 Col Photo picR3"></div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
